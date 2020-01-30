ae01fc050889947a58c3300fe1a0652337958f43
Baxter Bhansali <baxterb@internationalsf.org>
2020-01-30 23:16:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+n = 7
+import turtle
+turtle.speed(0)
+turtle.penup()
+steps = 10
+dist = 50
+
+def drawHorn(x, y, n):
+  if(n > 0):
+    turtle.setpos(x, y)
+    turtle.pendown()
+    turtle.forward(dist)
+    p = turtle.pos()
+    drawHorn(p[0], p[1], n - 1)
+    turtle.pendown()
+    turtle.left(180)
+    turtle.forward(dist/2)
+    turtle.left(210)
+    turtle.forward(dist/2)
+    p = turtle.pos()
+    drawHorn(p[0], p[1], n - 1)
+    turtle.penup()
+    turtle.setpos(x, y)
+    turtle.right(30)
+
+drawHorn(-200, 0, n)
