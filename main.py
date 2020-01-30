f69ce362015eb6c8a9909fba923fef87a50a3097
bCubed3 <baxterb@internationalsf.org>
2020-01-30 23:28:33 +0000

    not working, sequential gens

--- main.py
+++ main.py
@@ -5,22 +5,27 @@ turtle.penup()
 steps = 10
 dist = 50
 
-def drawHorn(x, y, n):
+def drawHorn(x, y, n, a=0):
   if(n > 0):
+    pos = []
     turtle.setpos(x, y)
     turtle.pendown()
     turtle.forward(dist)
-    p = turtle.pos()
-    drawHorn(p[0], p[1], n - 1)
+    pos.append(turtle.pos())
     turtle.pendown()
     turtle.left(180)
     turtle.forward(dist/2)
     turtle.left(210)
     turtle.forward(dist/2)
-    p = turtle.pos()
-    drawHorn(p[0], p[1], n - 1)
+    pos.append(turtle.pos())
     turtle.penup()
     turtle.setpos(x, y)
     turtle.right(30)
+    px = pos[0][0]
+    py = pos[0][1]
+    drawHorn(px, py, n - 1, turtle.towards(px, py))
+    px = pos[1][0]
+    py = pos[1][1]
+    drawHorn(px, py, n - 1, turtle.towards(px, py))
 
 drawHorn(-200, 0, n)
