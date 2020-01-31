363764f37ff29fe2a5e8665731e85dbf3f12ca5e
bCubed3 <baxterb@internationalsf.org>
2020-01-31 18:26:59 +0000

    order by generations [WORKING]

--- main.py
+++ main.py
@@ -5,21 +5,27 @@ turtle.penup()
 steps = 10
 dist = 50
 
-def drawHorn(x, y, n, pos, a=0):
+def drawHorn(x, y, n, pos=[], a=0):
   if(n > 0):
     turtle.setpos(x, y)
+    turtle.seth(a)
     turtle.pendown()
     turtle.forward(dist)
-    pos.append(turtle.pos())
+    pos.append((turtle.pos(), turtle.heading()))
     turtle.pendown()
     turtle.left(180)
     turtle.forward(dist/2)
     turtle.left(210)
     turtle.forward(dist/2)
-    pos.append(turtle.pos())
+    pos.append((turtle.pos(), turtle.heading()))
     turtle.penup()
     turtle.setpos(x, y)
     turtle.right(30)
     return pos
 
-print(drawHorn(-200, 0, n, []))
+pos = drawHorn(-200, 0, n)
+for j in range(n):
+  posn = []
+  for i in pos:
+    posn = posn + drawHorn(i[0][0], i[0][1], 1, [], i[1])
+  pos = posn
