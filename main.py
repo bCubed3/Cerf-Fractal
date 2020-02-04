f3f3257e530dcd7f81806ff70e9b84217efb772f
bCubed3 <baxterb@internationalsf.org>
2020-02-04 21:40:24 +0000

    fixed it !!!

--- main.py
+++ main.py
@@ -4,20 +4,32 @@ import turtle
 turtle.speed(0)
 turtle.penup()
 steps = 10
-dist = 100
+dist = 30
 
 def draw_fractal(x, y, n, a=0):
-	turtle.penup()
 	turtle.setpos(x, y)
-	turtle.seth(a)
 	if n == 1:
-    	turtle.pendown()
-    	turtle.forward(dist)
+		turtle.pendown()
+		turtle.forward(dist)
 		turtle.penup()
+		return turtle.pos()
 	if n == 2:
-    	turtle.pendown()
-		turtle.forward(dist/4)
-	draw_fractal(x, y, n - 1)
-	draw_fractal(x, y, n - 2)
+		draw_fractal(x, y, 1)
+		turtle.pendown()
+		turtle.left(45)
+		turtle.forward(dist / 3)
+		turtle.backward(dist / 3)
+		turtle.right(45)
+		turtle.forward(dist / 3)
+		turtle.penup()	
+		return turtle.pos()
+	if n > 2:
+		p = draw_fractal(x, y, n - 1)
+		turtle.left(45)
+		draw_fractal(p[0], p[1], n - 2)
+		turtle.right(45)
+		draw_fractal(p[0], p[1], n - 2)
+		return turtle.pos()
 
-draw_fractal(0, 0, 3)
+
+draw_fractal(-250, 0, 8)
